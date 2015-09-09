2db46aff4a62304e9a4f00c4b11bd2277a442672
Jörn Hoffmann <j.hoffmann@xceeth.com>
2015-09-09 17:10:17 +0200

    Restructure files

---
+++ examples/01.Basics/HelloWorld/HelloWorld.ino
@@ -0,0 +1,13 @@
+void setup() 
+{
+  // Configure the serial interface to 9600 Baud
+  Serial.begin(9600);
+
+  // Prints a string and begins with a new line
+  Serial.println("Hello, World!");
+}
+
+void loop() 
+{ }
+
+
