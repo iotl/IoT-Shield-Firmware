5bb2fe80d411be3babfb2092c3ae8525865ac624
Jörn Hoffmann <j.hoffmann@xceeth.com>
2015-09-10 13:55:35 +0200

    Refactor
    - rename LedFlash to LEDTask
    - seperate Class to own dictionary

--- examples/03.Scheduler/ToggleLedsScheduler/ToggleLedsScheduler.ino
+++ examples/03.Scheduler/ToggleLedsScheduler/ToggleLedsScheduler.ino
@@ -1,34 +1,17 @@
 #include <ParkingShield.h>
 #include <Scheduler.h>
-
-class LedFlash : public Task
-{
-  public:
-  LedFlash(ParkingShield::led_t led, ParkingShield &shield) : _led(led), _shield(shield)
-  {}
-  
-  void update(Scheduler &sched)
-  {
-    _shield.setLed(_led, _enable);
-    _enable = !_enable;
-  }
-
-  private:
-    ParkingShield::led_t _led;
-    ParkingShield &_shield;
-    bool _enable;
-};
-
+#include "LedTask.h"
 
 ParkingShield shield;
 Scheduler sched;
-LedFlash redFlash(ParkingShield::RED_LED, shield);
-LedFlash yellowFlash(ParkingShield::YELLOW_LED, shield);
+
+LedTask redTask(ParkingShield::RED_LED, shield);
+LedTask yellowTask(ParkingShield::YELLOW_LED, shield);
 
 void setup() 
 {
-  sched.addTask(&redFlash, 1000, true);
-  sched.addTask(&yellowFlash, 500, true);    
+  sched.addTask(&redTask, 1000, true);
+  sched.addTask(&yellowTask, 500, true);    
 }
 
 void loop()
